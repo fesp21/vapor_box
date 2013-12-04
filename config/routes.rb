e72827b85ee1afb267b246518697b3a823c24a0c
David Kim <dasaki@gmail.com>
2013-12-03 21:14:04 -0800

    add footer skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Myapp::Application.routes.draw do
 
   get '/about', to: 'static_pages#about'
   get '/how_it_works', to: 'static_pages#how_it_works'
+  get '/signup', to: 'static_pages#how_it_works'
   get "static_pages/home"
   
 
