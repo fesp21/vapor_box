02c8bcbc19e366e5e2f4732b609a1193dce18bec
David Kim <dasaki@gmail.com>
2014-01-22 10:07:21 -0800

    db changes

--- app/models/one_time_accessory.rb
+++ app/models/one_time_accessory.rb
@@ -1,5 +1,5 @@
 class OneTimeAccessory < ActiveRecord::Base
-  attr_accessible :subscription_id, :amount, :accessory_id
+  attr_accessible :subscription_id, :quantity, :accessory_id
   belongs_to :subscription
   has_one :accessory
   
--- db/migrate/20140121014526_create_one_time_accessories_table.rb
+++ db/migrate/20140121014526_create_one_time_accessories_table.rb
@@ -3,7 +3,7 @@ class CreateOneTimeAccessoriesTable < ActiveRecord::Migration
     create_table :one_time_accessories do |t|
       t.integer :subscription_id
       t.integer :accessory_id
-      t.integer :amount
+      t.integer :quantity
       t.timestamps
     end
     create_table :subscription_charges do |t|
