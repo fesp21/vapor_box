3f1454f6fdaf21a8ad855a1d41d9e065268146f9
David Kim <dasaki@gmail.com>
2014-01-27 11:44:23 -0800

    remove extra

--- db/migrate/20131210225141_plans_subscriptions_join_table.rb
+++
@@ -1,9 +0,0 @@
-class PlansSubscriptionsJoinTable < ActiveRecord::Migration
-  def change
-    create_table :plans_subscriptions, :id => false do |t|
-    t.integer :plan_id
-    t.integer :subscription_id
-    t.integer :quantity
-    end
-  end
-end
